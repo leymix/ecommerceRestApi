9477892889eeb3bd3523d5ae4e2bafaa988942b4
leymix <106842141+leymix@users.noreply.github.com>
2023-04-20 13:13:16 +0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,48 @@
-const express=require("express");
-const app=express();
-app.get('/',(req,res)=>{
-    res.send('hello API !')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+const port=process.env.API_PORT || 3001;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'ecommerceRestApi'
 })
-app.listen(3000,()=>{
-    console.log("server is running http://localhost:3000");
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
+//Server
+app.listen(port, ()=>{
+
+    console.log('server is running http://localhost:3000');
 })
